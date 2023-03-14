15c93f7dbe8d1b6d6a892e2619cdfaf70f03ca14
Miguel <99116773+styhrd@users.noreply.github.com>
2023-03-14 23:09:16 +0800

    LE4-CAPARAS

---
+++ CaparasBlogDB/Stored Procedures/spPosts_Insert.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+	@UserId int,
+	@title nvarchar(150),
+	@body text,
+	@dateCreated datetime2
+AS
+begin
+	INSERT INTO dbo.Posts
+	(UserId, Title, Body, DateCreated)
+	VALUES
+	(@UserId, @title, @body, @dateCreated)
+end
